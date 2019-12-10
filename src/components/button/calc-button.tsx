5c1d4674bfaac37209d6b70e6c23c3af83895456
Анастасия Войценя <vojtsenyaas@it2g.ru>
2019-12-10 18:42:38 +0300

    wip

--- src/components/button/calc-button.tsx
+++ src/components/button/calc-button.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {SyntheticEvent} from "react";
 import './calc-button.scss'
 
 interface ButtonProps {
     name: string;
     class?: ClassType;
+    clickMe?: (val: string | number) => void;
 }
 
 type ClassType = 'dark' | 'light';
@@ -14,8 +15,17 @@ export const CalcButton: React.FC<ButtonProps> = (props: ButtonProps) => {
         return classType === 'dark' ? 'calc-button-primary calc-button' : 'calc-button-secondary calc-button';
     };
 
+    const handleClick = (e: SyntheticEvent) => {
+        e.preventDefault();
+
+        if(props.clickMe) {
+            return props.clickMe(props.name);
+        }
+
+        return;
+    };
 
     return (
-        <div className={getClass(props.class)}>{props.name}</div>
+        <div onClick={handleClick} className={getClass(props.class)}>{props.name}</div>
     );
 };
--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -1,37 +1,80 @@
-import React from "react";
+import React, {useState} from "react";
 import './grid.scss';
 import {CalcButton} from "../button/calc-button";
 
+type CounterState = number | string;
+
 export const Grid: React.FC = () => {
+    const [count, setCount] = useState<CounterState[] | any>(null);
+
+    const addEvent = (val: string | number) => {
+
+        if (count && count.length > 0) {
+           const newCount = [...count, val];
+            setCount(newCount);
+        } else {
+            setCount([val]);
+        }
+
+    };
+
+    const clearEvent = () => {
+        setCount([]);
+    };
+
+    const removeLast = () => {
+        if (count) {
+            const l = count.length;
+            const newCount = l && l > 0 ? count.splice(0, (l - 1)) : [];
+            setCount(newCount);
+        }
+    };
+
+    function getResult() {
+
+        const result =  count && count.reduce((acc: any, item: CounterState) => {
+            if (typeof item === 'string') {
+
+            }
+            if (typeof item === 'number') {
+
+            }
+
+            return `${acc}, ${item}`
+        }, 0);
+
+        setCount(result);
+    }
+
     return (
         <div className={'wrapper'}>
-            <div className={'display'}>12345</div>
+            <div className={'display'}>{count}</div>
             <div className={'grid'}>
-                <CalcButton class={'light'} name={'C'}/>
-                <CalcButton name={'+/-'}/>
-                <CalcButton name={'%'}/>
-                <CalcButton class={'light'} name={'DEL'}/>
+                <CalcButton clickMe={(val) => clearEvent()} class={'light'} name={'C'}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'+/-'}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'%'}/>
+                <CalcButton clickMe={(val) => removeLast()} class={'light'} name={'DEL'}/>
 
-                <CalcButton name={'7'}/>
-                <CalcButton name={'8'}/>
-                <CalcButton name={'9'}/>
-                <CalcButton name={'÷'}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'7'}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'8'}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'9'}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'÷'}/>
 
-                <CalcButton name={'4'}/>
-                <CalcButton name={'5'}/>
-                <CalcButton name={'6'}/>
-                <CalcButton name={'×'}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'4'}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'5'}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'6'}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'×'}/>
 
 
-                <CalcButton name={'1'}/>
-                <CalcButton name={'2'}/>
-                <CalcButton name={'3'}/>
-                <CalcButton name={'-'}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'1'}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'2'}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'3'}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'-'}/>
 
-                <CalcButton name={'0'}/>
-                <CalcButton name={'.'}/>
-                <CalcButton class={'light'} name={'='}/>
-                <CalcButton name={'+'}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'0'}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'.'}/>
+                <CalcButton clickMe={(val) => getResult()} class={'light'} name={'='}/>
+                <CalcButton clickMe={(val) => addEvent(val)} name={'+'}/>
             </div>
         </div>
 
